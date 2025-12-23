ebbe12487430deac3a3d66a1fd716f466fa137fe
dami <dami@debian.debian.com>
2025-12-23 11:29:34 -0500

    Decode terminal JSON as UTF-8

--- src/darelwasl/terminal/client.clj
+++ src/darelwasl/terminal/client.clj
@@ -6,10 +6,20 @@
 
 (def ^:private default-timeout-ms 5000)
 
+(defn- response-body->string
+  [body]
+  (cond
+    (nil? body) nil
+    (string? body) body
+    (instance? (Class/forName "[B") body) (String. ^bytes body "UTF-8")
+    (instance? java.io.InputStream body) (slurp body :encoding "UTF-8")
+    :else (str body)))
+
 (defn- parse-json
   [body]
-  (when (and body (not (str/blank? body)))
-    (json/read-str body :key-fn keyword)))
+  (let [text (some-> body response-body->string)]
+    (when (and text (not (str/blank? text)))
+      (json/read-str text :key-fn keyword))))
 
 (defn request
   [cfg method path & [body]]
@@ -23,6 +33,7 @@
                                 :conn-timeout default-timeout-ms
                                 :headers {"Accept" "application/json"}
                                 :content-type :json
+                                :as :byte-array
                                 :body (when body (json/write-str body))})
             status (:status resp)
             parsed (parse-json (:body resp))]
